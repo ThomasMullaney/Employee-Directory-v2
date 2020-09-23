72473035dbbf0086970d72666a95ea364c9d30e2
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-09-22 17:14:38 -0700

    Updates

---
+++ precache-manifest.18bc23800543fb90e6e6a8eba7b709e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f499fd1a2f6afd92cb4330103439a7",
+    "url": "/Employee-Directory-v2/index.html"
+  },
+  {
+    "revision": "1b48bd830532df722b7c",
+    "url": "/Employee-Directory-v2/static/css/main.91db2b32.chunk.css"
+  },
+  {
+    "revision": "42f47817c5c2026ae05f",
+    "url": "/Employee-Directory-v2/static/js/2.0fa8f49e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory-v2/static/js/2.0fa8f49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b48bd830532df722b7c",
+    "url": "/Employee-Directory-v2/static/js/main.a068ce9d.chunk.js"
+  },
+  {
+    "revision": "38c4656940ad9998e47c",
+    "url": "/Employee-Directory-v2/static/js/runtime-main.bbcb564d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Employee-Directory-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
